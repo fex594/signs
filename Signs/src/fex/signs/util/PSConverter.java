10dcd73f4be06f51c7f760f32d7519b3b2b62658
Felix <fex>
2018-10-15 15:58:39 +0200

    Added PSConverter to convert between ResultSet and PlayerSign

---
+++ Signs/src/fex/signs/util/PSConverter.java
@@ -0,0 +1,26 @@
+package fex.signs.util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PSConverter {
+	public static PlayerSign convert(ResultSet r) {
+		try {
+		int ID = r.getInt("ID");
+		String besitzerUUID = r.getString("Player");
+		java.sql.Date ablaufDatum = r.getDate("Datum");
+		int active = r.getInt("Active");
+		String location = r.getString("Loc");
+		String type = r.getString("Typ");
+		String erstellerUUID = r.getString("Ersteller");
+		java.sql.Date maxExpandDate = r.getDate("Lastdate");
+		
+		PlayerSign ps = new PlayerSign(ID, besitzerUUID, ablaufDatum, active, location, type, erstellerUUID, maxExpandDate);
+		return ps;
+		
+		}catch(SQLException e) {
+			System.out.println("Fehler beim Konvertieren des Results in Schilddaten");
+		}
+		return null;
+	}
+}
--- Signs/src/fex/signs/util/PlayerSign.java
+++ Signs/src/fex/signs/util/PlayerSign.java
@@ -1,5 +1,60 @@
 package fex.signs.util;
 
+import java.sql.Date;
+
 public class PlayerSign {
+	private int ID;
+	private String besitzerUUID;
+	private java.sql.Date ablaufDatum;
+	private int active;
+	private String location;
+	private String type;
+	private String erstellerUUID;
+	private java.sql.Date maxExpandDate;
+	
+	public PlayerSign(int ID, String besitzerUUID, Date ablaufDatum, int active, String location, String type,
+			String erstellerUUID, Date maxExpandDate) {
+		this.ID = ID;
+		this.besitzerUUID = besitzerUUID;
+		this.ablaufDatum = ablaufDatum;
+		this.active = active;
+		this.location = location;
+		this.type = type;
+		this.erstellerUUID = erstellerUUID;
+		this.maxExpandDate = maxExpandDate;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public String getBesitzerUUID() {
+		return besitzerUUID;
+	}
+
+	public java.sql.Date getAblaufDatum() {
+		return ablaufDatum;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getErstellerUUID() {
+		return erstellerUUID;
+	}
+
+	public java.sql.Date getMaxExpandDate() {
+		return maxExpandDate;
+	};
+	
 	
 }
