b908c50a796e9faa157a6134b48cb4f20c367483
Schäbler, Felix <felix.schaebler@student.fhws.de>
2019-08-15 21:29:40 +0200

    Updated to Spigot 1.14.4 + fix buggy project

--- Signs/src/fex/signs/util/Util.java
+++ Signs/src/fex/signs/util/Util.java
@@ -2,8 +2,8 @@ package fex.signs.util;
 
 import java.sql.Date;
 import java.util.Calendar;
-import java.util.HashSet;
-import java.util.Set;
+//import java.util.HashSet;
+//import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
@@ -12,40 +12,43 @@ import org.bukkit.Material;
 
 public class Util {
 	public static boolean isSign(Material m){
-		Set<Material> signTypes = new HashSet<>();
-		signTypes.add(Material.ACACIA_SIGN);
-		signTypes.add(Material.ACACIA_WALL_SIGN);
-		signTypes.add(Material.SPRUCE_SIGN);
-		signTypes.add(Material.SPRUCE_WALL_SIGN);
-		signTypes.add(Material.OAK_SIGN);
-		signTypes.add(Material.OAK_WALL_SIGN);
-		signTypes.add(Material.DARK_OAK_SIGN);
-		signTypes.add(Material.DARK_OAK_WALL_SIGN);
-		signTypes.add(Material.JUNGLE_SIGN);
-		signTypes.add(Material.JUNGLE_WALL_SIGN);
-		signTypes.add(Material.BIRCH_SIGN);
-		signTypes.add(Material.BIRCH_WALL_SIGN);
-		return signTypes.contains(m);
+		return m.toString().endsWith("_SIGN");
+//		Set<Material> signTypes = new HashSet<>();
+//		signTypes.add(Material.ACACIA_SIGN);
+//		signTypes.add(Material.ACACIA_WALL_SIGN);
+//		signTypes.add(Material.SPRUCE_SIGN);
+//		signTypes.add(Material.SPRUCE_WALL_SIGN);
+//		signTypes.add(Material.OAK_SIGN);
+//		signTypes.add(Material.OAK_WALL_SIGN);
+//		signTypes.add(Material.DARK_OAK_SIGN);
+//		signTypes.add(Material.DARK_OAK_WALL_SIGN);
+//		signTypes.add(Material.JUNGLE_SIGN);
+//		signTypes.add(Material.JUNGLE_WALL_SIGN);
+//		signTypes.add(Material.BIRCH_SIGN);
+//		signTypes.add(Material.BIRCH_WALL_SIGN);
+//		return signTypes.contains(m);
 	}
 	public static boolean isStandingSign(Material m){
-		Set<Material> signTypes = new HashSet<>();
-		signTypes.add(Material.ACACIA_SIGN);
-		signTypes.add(Material.SPRUCE_SIGN);
-		signTypes.add(Material.OAK_SIGN);
-		signTypes.add(Material.DARK_OAK_SIGN);
-		signTypes.add(Material.JUNGLE_SIGN);
-		signTypes.add(Material.BIRCH_SIGN);
-		return signTypes.contains(m);
+		return (m.toString().endsWith("_SIGN") && !m.toString().contains("WALL"));
+//		Set<Material> signTypes = new HashSet<>();
+//		signTypes.add(Material.ACACIA_SIGN);
+//		signTypes.add(Material.SPRUCE_SIGN);
+//		signTypes.add(Material.OAK_SIGN);
+//		signTypes.add(Material.DARK_OAK_SIGN);
+//		signTypes.add(Material.JUNGLE_SIGN);
+//		signTypes.add(Material.BIRCH_SIGN);
+//		return signTypes.contains(m);
 	}
 	public static boolean isWallSign(Material m){
-		Set<Material> signTypes = new HashSet<>();
-		signTypes.add(Material.ACACIA_WALL_SIGN);
-		signTypes.add(Material.SPRUCE_WALL_SIGN);
-		signTypes.add(Material.OAK_WALL_SIGN);
-		signTypes.add(Material.DARK_OAK_WALL_SIGN);
-		signTypes.add(Material.JUNGLE_WALL_SIGN);
-		signTypes.add(Material.BIRCH_WALL_SIGN);
-		return signTypes.contains(m);
+		return m.toString().endsWith("_WALL_SIGN");
+//		Set<Material> signTypes = new HashSet<>();
+//		signTypes.add(Material.ACACIA_WALL_SIGN);
+//		signTypes.add(Material.SPRUCE_WALL_SIGN);
+//		signTypes.add(Material.OAK_WALL_SIGN);
+//		signTypes.add(Material.DARK_OAK_WALL_SIGN);
+//		signTypes.add(Material.JUNGLE_WALL_SIGN);
+//		signTypes.add(Material.BIRCH_WALL_SIGN);
+//		return signTypes.contains(m);
 	}
 	public static boolean isSign(Location e) {
 		return isSign(e.getBlock().getType());
